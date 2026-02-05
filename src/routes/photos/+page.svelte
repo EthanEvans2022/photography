74803c12e186ea1ce0cc98950188efc21d13be4a
Ethan Evans <ethanevans622@gmail.com>
2026-02-05 13:41:51 -0500

    made PhotoWrapper component

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
+	import PhotoWrapper from '$lib/components/PhotoWrapper.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 </script>
 
 <div class="photos-container">
 	{#each data.photos as photo}
-		<div
-			class="photo-wrapper"
-			class:landscape={photo.width > photo.height}
-			class:portrait={photo.height >= photo.width}
-		>
-			<img src={photo.src} alt={photo.alt ?? ''} />
-		</div>
+		<PhotoWrapper {photo} />
 	{/each}
 </div>
 
@@ -22,24 +17,5 @@
 		flex-wrap: wrap;
 		gap: 1rem;
 		padding: 1rem;
-	}
-
-	.photo-wrapper {
-		flex-shrink: 0;
-	}
-
-	.photo-wrapper.landscape {
-		height: 300px;
-	}
-
-	.photo-wrapper.portrait {
-		height: 400px;
-	}
-
-	.photo-wrapper img {
-		height: 100%;
-		width: auto;
-		object-fit: contain;
-		border-radius: 4px;
 	}
 </style>
